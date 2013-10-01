2442ebc79c0b57a0a2a250379cf6e2cae4b3a88b
CottetE <emeraldcottet@gmail.com>
2013-10-01 16:17:46 +0200

    Update zones-de-couleurs-validation.py

--- niveau-01/chapitre-7-conditions-avancees-operateurs-booleens/zones-de-couleurs-validation.py
+++ niveau-01/chapitre-7-conditions-avancees-operateurs-booleens/zones-de-couleurs-validation.py
@@ -1,4 +1,3 @@
-
 ##################################
 # fichier zones-de-couleurs-validation.py
 # nom de l'exercice :  Zones de couleurs
@@ -16,4 +15,16 @@
 
 
 # mettre votre code ici
+nbrjoueur=int(input())
+for i in range(nbrjoueur):
+    jX=int(input())
+    jY=int(input())
 
+    if (jX<0 or jX>90)or (jY<0 or jY>70):
+        print("En dehors de la feuille")
+    elif (15<jX<45 and 60<jY<70)or(60<jX<85 and 60<jY<70):
+        print("Dans une zone rouge")
+    elif (10<jX<85 and 10<jY<55 )and not (25<jX<50 and 20<jY<45):
+        print("Dans une zone bleue")
+    else:
+        print("Dans une zone jaune")
