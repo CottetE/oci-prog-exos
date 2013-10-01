ab99e5d73876a23f96f7500930a1274ace6fdbf0
CottetE <emeraldcottet@gmail.com>
2013-10-01 15:55:42 +0200

    Update bonus--casernes-de-pompiers-validation.py

--- niveau-01/chapitre-7-conditions-avancees-operateurs-booleens/bonus--casernes-de-pompiers-validation.py
+++ niveau-01/chapitre-7-conditions-avancees-operateurs-booleens/bonus--casernes-de-pompiers-validation.py
@@ -1,4 +1,3 @@
-
 ##################################
 # fichier bonus--casernes-de-pompiers-validation.py
 # nom de l'exercice :  Bonus : Casernes de pompiers
@@ -16,4 +15,24 @@
 
 
 # mettre votre code ici
+nbrpaires=int(input())
+for i in range(nbrpaires):
+    absmin1=int(input())
+    absmax1=int(input())
+    ordmin1=int(input())
+    ordmax1=int(input())
+    absmin2=int(input())
+    absmax2=int(input())
+    ordmin2=int(input())
+    ordmax2=int(input())
 
+    if absmax1<=absmin2 or absmax2<=absmin1:
+        print("NON")
+    elif absmin1>=absmax2 or absmin2>=absmax1:
+        print("NON")
+    elif ordmax1<=ordmin2 or ordmax2<=ordmin1:
+        print("NON")
+    elif ordmin1>=ordmax2 or ordmin2>=ordmax1:
+        print("NON")
+    else:
+        print("OUI")
