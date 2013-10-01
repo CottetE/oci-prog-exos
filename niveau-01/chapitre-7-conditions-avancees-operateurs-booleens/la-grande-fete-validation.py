87787409c70964297684be9cd0d6e5b893cee1ed
CottetE <emeraldcottet@gmail.com>
2013-10-01 16:43:04 +0200

    Update la-grande-fete-validation.py

--- niveau-01/chapitre-7-conditions-avancees-operateurs-booleens/la-grande-fete-validation.py
+++ niveau-01/chapitre-7-conditions-avancees-operateurs-booleens/la-grande-fete-validation.py
@@ -1,4 +1,3 @@
-
 ##################################
 # fichier la-grande-fete-validation.py
 # nom de l'exercice :  La grande fête
@@ -16,4 +15,15 @@
 
 
 # mettre votre code ici
-
+datemin=int(input())
+datemax=int(input())
+nbrinvite=int(input())
+a=0
+for i in range(nbrinvite):
+    date1=int(input())
+    date2=int(input())
+    if datemin<=date1<=datemax or datemin<=date2<=datemax:
+        a+=1
+    elif date1<=datemin<=date2 or date1<=datemax<=date2:
+        a+=1
+print(a)
