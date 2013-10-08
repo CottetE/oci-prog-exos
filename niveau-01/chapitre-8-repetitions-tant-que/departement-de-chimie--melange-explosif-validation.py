2a57356900f3826591908ab3a07be05a3302cdf4
CottetE <emeraldcottet@gmail.com>
2013-10-08 16:00:15 +0200

    Update departement-de-chimie--melange-explosif-validation.py

--- niveau-01/chapitre-8-repetitions-tant-que/departement-de-chimie--melange-explosif-validation.py
+++ niveau-01/chapitre-8-repetitions-tant-que/departement-de-chimie--melange-explosif-validation.py
@@ -1,4 +1,3 @@
-
 ##################################
 # fichier departement-de-chimie--melange-explosif-validation.py
 # nom de l'exercice :  Département de chimie : mélange explosif
@@ -16,4 +15,16 @@
 
 
 # mettre votre code ici
+nbrMesures=int(input())
+tempMin=int(input())
+tempMax=int(input())
+i=0
+while i<nbrMesures:
+    temperature=int(input())
+    if tempMin<=temperature<=tempMax:
+        print("Rien à signaler")
+    else:
+        print("Alerte !!")
+        break
+    i+=1
 
