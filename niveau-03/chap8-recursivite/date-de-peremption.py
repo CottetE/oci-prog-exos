8ee5bcdb391c28af4a1eabe9ab923770c0f0af4d
cottete <emeraldcottet@gmail.com>
2014-12-30 13:14:52 +0000

    bravo tu sais pas lire les consignes

--- niveau-03/chap8-recursivite/date-de-peremption.py
+++ niveau-03/chap8-recursivite/date-de-peremption.py
@@ -1,4 +1,23 @@
-def operations(n):
+def operation(n):
     for i in range(n):
-        nbr,date = int(input().split())
-        
+        nbr, date = map(int,input().split())
+        if nbr < 0:
+           nbr = -nbr
+        for j in range(nbr):
+            if date == 0:
+                del(liste[-1])
+            else:
+                liste.append(date)
+                
+    print(min(liste))       #merci à Simon
+
+pile = []
+n = int(input())
+operation(n)
+
+#si j'avais bien lui ce qui suit:
+
+#Quand Gérard renouvelle son stock, il n'a en général pas beaucoup de temps et 
+#place les nouveaux produits juste au dessus de ceux qui sont déjà en rayon.
+
+#je n'aurais pas pris 3h pour cet exercice...
